d16e53673698688944d8a6914e30f2bc3873b49d
lenh0205 <lenhdeptrai98@gmail.com>
2022-07-19 23:53:50 +0700

    17.post method

--- src/index.js
+++ src/index.js
@@ -1,34 +1,48 @@
-const path = require('path');
+const path = require("path");
 const express = require("express");
-const morgan = require('morgan');
-const { engine } = require('express-handlebars');
+const morgan = require("morgan");
+const { engine } = require("express-handlebars");
 
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
+
+// Đây là Middleware xử lý dữ liệu submit lên từ cái form HTML
+app.use(express.urlencoded({ 
+  extended: true 
+}));
+// Gửi dữ liệu từ Client lên Server = cách s/d code Js hoặc thư viện Js (XMLHttpRequest, fetch, axios, Ajax) để submit
+app.use(express.json());
 
 // HTTP logger
-app.use(morgan('combined'))
+app.use(morgan("combined"));
 
 // Template engine
-app.engine('hbs', engine({
-  extname: '.hbs'
-})) // định nghĩa handlebars
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'))
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+  })
+); // định nghĩa handlebars
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
 // console.log(__dirname, path.join(__dirname, 'resources/views))
 
 app.get("/", (req, res) => {
-  res.render('home');
+  res.render("home");
 });
 
 app.get("/news", (req, res) => {
-  res.render('news');
+  res.render("news");
 });
 
 app.get("/search", (req, res) => {
-  res.render('search');
+  res.render("search");
+});
+app.post("/search", (req, res) => {
+  console.log(req.body); // Để lấy Form Data
+  res.send("");
 });
 
 app.listen(port, () => {
