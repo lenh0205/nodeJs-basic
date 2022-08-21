a79624fd20363744b3d35684e2799f7af203b3ac
lenh0205 <lenhdeptrai98@gmail.com>
2022-08-21 10:33:54 +0700

    Course detail page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
 const newsRouter = require("./news");
+const coursesRouter = require("./courses");
 const siteRouter = require("./site");
 
 function route(app) {
   app.use("/news", newsRouter);
   // express sẽ hiều đối số thứ 1 là tuyến đường; đối số thứ 2 là cấu hình các tuyến con cho news
+
+  app.use("/courses", coursesRouter);
 
   app.use("/", siteRouter);
 }
