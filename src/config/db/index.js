e2fff50f0aaf97e919083d2623081331e233201a
tranchilenhphotobank <110368991+tranchilenhphotobank@users.noreply.github.com>
2023-01-27 16:54:06 +0700

    [CRUD] Delete course

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose')
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/express_basic_dev');
+        await mongoose.connect('mongodb://localhost:27017/express_basic_dev'
+            // ,{
+            //     useNewUrlParser: true,
+            //     useUnifiedTopology: true,
+            //     useCreateIndex: true,
+            // }
+        );
         console.log('connect success')
     } catch (error) {
         console.log('connect fail')
